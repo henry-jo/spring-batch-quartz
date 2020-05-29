6282e9a9c8bba05232ff919a74579e383f9ed52e
henryjo <jobc0335@gmail.com>
2020-05-29 13:52:06 +0900

    init project

---
+++ src/main/java/com/henry/springbatchquartz/SpringBatchQuartzApplication.kt
@@ -0,0 +1,11 @@
+package com.henry.springbatchquartz
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBatchQuartzApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBatchQuartzApplication::class.java, *args)
+}
---
+++ src/test/java/com/henry/springbatchquartz/SpringBatchQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.springbatchquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
